152e6670d34b194529f9a03666cff492a0679586
Iasmine <Iasmine948@gmail.com>
2024-06-11 20:16:12 -0300

    tipos de operadores

---
+++ basico/operadores.js
@@ -0,0 +1,19 @@
+let num1 =3
+let num2 = 2
+let num3 = 3
+let driver = 90
+let speed = driver > 110 ? 'Acima' : 'Abaixo'
+
+console.log(num1 + num2)
+console.log(total)
+
+console.log(num1 > 0)
+console.log(num1 >=3)
+
+console.log(num1 === 3)
+console.log(speed)
+
+
+
+
+
