215d8758786fdf0312aabecc0f591bf2102ce55c
Iasmine <Iasmine948@gmail.com>
2024-11-09 19:41:04 -0300

    Formação QA Alura

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert('Olá Mundo');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número etre 1 e 30');
+if (numeroSecreto == chute) {
+    console.log('Isso ai você descobriu o número secreto (5)');
+}
